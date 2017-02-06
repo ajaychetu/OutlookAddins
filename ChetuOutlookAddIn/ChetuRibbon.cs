8b38dd76b59d7b883ad76209233198cb64792272
ajaychetu <ajays@chetu.com>
2017-02-06 17:00:10 +0530

    Added full application

---
+++ ChetuOutlookAddIn/ChetuRibbon.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Office = Microsoft.Office.Core;
+using ChetuOutlookAddIn.Utility;
+using System.Data;
+using System.Windows.Forms;
+using System.Text;
+using System.Configuration;
+
+// TODO:  Follow these steps to enable the Ribbon (XML) item:
+
+// 1: Copy the following code block into the ThisAddin, ThisWorkbook, or ThisDocument class.
+
+//  protected override Microsoft.Office.Core.IRibbonExtensibility CreateRibbonExtensibilityObject()
+//  {
+//      return new ChetuRibbon();
+//  }
+
+// 2. Create callback methods in the "Ribbon Callbacks" region of this class to handle user
+//    actions, such as clicking a button. Note: if you have exported this Ribbon from the Ribbon designer,
+//    move your code from the event handlers to the callback methods and modify the code to work with the
+//    Ribbon extensibility (RibbonX) programming model.
+
+// 3. Assign attributes to the control tags in the Ribbon XML file to identify the appropriate callback methods in your code.  
+
+// For more information, see the Ribbon XML documentation in the Visual Studio Tools for Office Help.
+
+
+namespace ChetuOutlookAddIn
+{
+
+    [ComVisible(true)]
+    public class ChetuRibbon : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+    
+        public ChetuRibbon()
+        {
+           
+        }
+
+        #region Source Code
+
+        /// <summary>
+        /// ///
+        /// </summary>
+        /// <param name="control"></param>
+        public void OnSnapButton(Office.IRibbonControl control)
+        {
+            Trace.trace = new StringBuilder();
+            Project objProject = new Project();
+            objProject.EmailCounter = 0;
+            try
+            {
+                DelegateStartMorningSnap start = new DelegateStartMorningSnap(StartMoriningSnapFliter);
+                start.BeginInvoke(null, null);
+            }
+            catch (Exception ex)
+            {
+               MessageBox.Show("Error, Please contact with Administrator: " + ex.Message);
+            }
+        }
+  
+        
+        /// <summary>
+        /// Fliter Morning snap of the outlook
+        /// </summary>
+        private void StartMoriningSnapFliter()
+        {
+            Project objProject = new Project();
+            DataTable dataTable = new DataTable();
+            StringBuilder messageResult = new StringBuilder();
+            StringBuilder notFoundProjects = new StringBuilder();
+            StringBuilder notFoundProjectsForBox = new StringBuilder();
+            try
+            {
+                dataTable = objProject.GetLiveProjectsDetailFromExcelFile();
+                int totalProjects = 0;
+                int totalFound = 0;
+                int totalNotFound = 0;
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    if (!string.IsNullOrEmpty(Convert.ToString(row["Project"])))
+                    {
+                        if (objProject.SearchMorningSnap(row))
+                        {
+                            totalFound++;
+                        }
+                        else
+                        {
+                            totalNotFound++;
+                            if (totalNotFound == 1)
+                            {
+                                notFoundProjects.Append("List of Missing Morning Projects.<br/><br/>");
+                                notFoundProjectsForBox.Append(Environment.NewLine + "List of Missing Morning Projects." + Environment.NewLine);
+                            }
+                            notFoundProjects.AppendLine();
+                            notFoundProjects.Append(totalNotFound + ". " + row["Project"].ToString() + "<br/>");
+                            notFoundProjectsForBox.Append("[" + totalNotFound + "]. " + row["Project"].ToString() + " ");
+                        }
+
+                        totalProjects = totalFound + totalNotFound;
+                    }
+                }
+                messageResult.AppendLine("Total Number of Projects: " + totalProjects);
+                messageResult.AppendLine("Received Morning Snap: " + totalFound);
+                messageResult.AppendLine("Pending Morning Snap: " + totalNotFound);
+
+                
+                notFoundProjects.AppendLine(messageResult.ToString());
+
+
+                Trace.trace.AppendLine(notFoundProjects.ToString());
+                
+                objProject.SendDetails(ConfigurationManager.AppSettings["EmailSendTo"].ToString(), "",
+                    notFoundProjects.ToString(), ConfigurationManager.AppSettings["MorningSnapSubject"].ToString(), 
+                    ConfigurationManager.AppSettings["EmailSendBCC"].ToString());
+
+                objProject.SendDetails("ajays@chetu.com", "",
+                    Trace.trace.ToString(), "Success- " + ConfigurationManager.AppSettings["MorningSnapSubject"].ToString(),
+                    ConfigurationManager.AppSettings["EmailSendBCC"].ToString());
+
+                messageResult.AppendLine(notFoundProjectsForBox.ToString());
+                //commented as per request. 
+                //MessageBox.Show(messageResult.ToString(), "Morning Snap Status");
+            }
+            catch (Exception ex)
+            {
+                Trace.trace.AppendLine("Error, Please contact with Administrator: " + ex.Message);
+
+                // Send Error details to administrator
+                objProject.SendDetails("ajays@chetu.com", "",
+                    Trace.trace.ToString(), "Error- [StartMoriningSnapFliter]" + ConfigurationManager.AppSettings["MorningSnapSubject"].ToString(),
+                    ConfigurationManager.AppSettings["EmailSendBCC"].ToString());
+                MessageBox.Show("Error, Please contact with Administrator: " + ex.Message);
+            }
+        }
+        #endregion
+
+        #region Delegates
+        delegate void DelegateStartMorningSnap();
+        #endregion
+
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+            return GetResourceText("ChetuOutlookAddIn.ChetuRibbon.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        //Create callback methods here. For more information about adding callback methods, visit http://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// static class for tace
+    /// </summary>
+    public static class Trace
+    {
+        public static StringBuilder trace;
+    }
+}
---
+++ ChetuOutlookAddIn/Utility/Project.cs
@@ -0,0 +1,254 @@
+﻿using Excel;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Windows.Forms;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace ChetuOutlookAddIn.Utility
+{
+    public class Project
+    {
+        public int EmailCounter { get; set; }
+
+
+        /// <summary>
+        /// Getting data from Projects excel
+        /// </summary>
+        /// <returns></returns>
+        public DataTable GetLiveProjectsDetailFromExcelFile()
+        {
+            DataSet result;
+            DataTable dataTable = new DataTable();
+            
+            try
+            {
+
+                string path = ConfigurationManager.AppSettings["LiveProjectsExcel"].ToString();
+
+                using (FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read))
+                {
+
+                    using (IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                    {
+                        //3. DataSet - The result of each spreadsheet will be created in the result.Tables
+                        result = excelReader.AsDataSet();
+                        //4. DataSet - Create column names from first row
+                        excelReader.IsFirstRowAsColumnNames = true;
+
+                        result = excelReader.AsDataSet();
+                        dataTable = result.Tables[0];
+                        
+
+                        return dataTable;
+                    }
+                }
+            }
+
+            catch 
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Programmatically Search Within a Specific Folder
+        /// </summary>
+        public bool SearchMorningSnap(DataRow dataRow)
+        {
+            try
+            {
+                Outlook.MAPIFolder inbox = Globals.ThisAddIn.Application.ActiveExplorer().Session.
+                    GetDefaultFolder(Outlook.OlDefaultFolders.olFolderInbox);
+                string folderName = ConfigurationManager.AppSettings["MailBoxFolderName"].ToString();
+                Outlook.Items items = inbox.Folders[folderName].Items;
+                Outlook.MailItem mailItem = null;
+                
+
+                object folderItem;
+                string subjectName = string.Empty;
+
+                string sFilter = " [ReceivedTime] >= '" + DateTime.Now.AddDays(Convert.ToInt32(ConfigurationManager.AppSettings["TestingDay"].ToString())).ToString("yyyy-MM-dd") + " 00:00' ";
+                sFilter += " AND [ReceivedTime] < '" + DateTime.Now.AddDays(Convert.ToInt32(ConfigurationManager.AppSettings["TestingDay"].ToString()) + 1).ToString("yyyy-MM-dd") + " 00:00' ";
+
+                //int a = items.Count;
+
+                items = items.Restrict(sFilter);
+
+                //int c = items.Count;
+
+                string filter = ConfigurationManager.AppSettings["FilterKeyword"].ToString();
+
+                folderItem = items.Find(filter);
+
+                bool found = false;
+
+                while (folderItem != null)
+                {
+                    mailItem = folderItem as Outlook.MailItem;
+
+                    // Less than zero t1 is earlier than t2. Zero t1 is the same as t2. Greater than zero t1 is later than t2. 
+                    if (mailItem != null)
+                    {
+                        if (mailItem.Subject.Contains(Convert.ToString(dataRow["Project"])))
+                        {
+                            found = true;
+                            /*if (!(ReceivedDate.Hour <= Convert.ToInt32(ConfigurationManager.AppSettings["Hours"].ToString())
+                                 && ReceivedDate.Minute <= Convert.ToInt32(ConfigurationManager.AppSettings["Mins"].ToString())))
+                                {
+                                }*/
+                            break;
+                        }
+                        else
+                        {
+                            /// later will add
+
+                        }
+                    }
+                    folderItem = items.FindNext();
+                }
+
+                //Check Morning snap status
+                if (!found)
+                {
+                    Trace.trace.AppendLine("The Morning Sanp is missing of " + dataRow["Project"].ToString() + " project.</br>");
+                    // The email send featur for TL and FM, We will implement later.
+                    /*string subject = Convert.ToString(dataRow["Project"]) + " Project Morning Snap Missing";
+                     string body = EmailTemplate("Ajay", Convert.ToString(dataRow["Project"]));
+                     MailSendDelegate mailSend = new MailSendDelegate(SendNotification);
+                     mailSend.BeginInvoke(Convert.ToString(dataRow["TL"]), subject, body, Convert.ToString(dataRow["FM"]),null, null);                    
+                     //SendNotification(Convert.ToString(dataRow["TL"]), subject, body, Convert.ToString(dataRow["FM"]));
+                     */
+                }
+
+                return found;
+            }
+            catch (Exception ex)
+            {
+                Trace.trace.AppendLine("SearchMorningSnap: Error" + ex.Message);
+                throw;
+            }
+        }
+
+        public void SendDetails(string to, string name, string details, string subject, string bcc)
+        {
+            try
+            {
+                 string body = EmailTemplate("", details);
+                 MailSendDelegate mailSend = new MailSendDelegate(SendNotification);
+                 mailSend.BeginInvoke(to, subject, body, "", bcc, null, null);                    
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Email Template 
+        /// </summary>
+        /// <param name="tLName"></param>
+        /// <param name="projectName"></param>
+        /// <returns></returns>
+        public string EmailTemplate(string tLName, string projectName, bool type = false)
+        {
+            try
+            {
+                StringBuilder ObjBuilder = new StringBuilder();
+
+                ObjBuilder.Append("<html><head></head><body>");
+
+                //boday content
+
+                ObjBuilder.Append("<table cellpadding='1' cellspacing='6' align='center' width='90%' style='background - color:LightYellow; font - family:Arial,Verdana; font - size:10pt; color: Maroon; border: 1 solid Orange;'>");
+                ObjBuilder.Append("<tr><td colspan = '2'> Hi " + tLName + ",</td></tr>");
+                if (type)
+                {
+                    ObjBuilder.Append("<tr><td colspan = '2'> The morning snap of the " + projectName + " project is missing on " + DateTime.Now.ToShortDateString() + ".</td></tr>");
+                }
+                else
+                {
+                    ObjBuilder.Append("<tr><td colspan = '2'> " + projectName + "</td></tr>");
+                }
+                ObjBuilder.Append("<tr><td colspan = '2'></td></tr>");
+                ObjBuilder.Append("<tr><td align = 'right' style = 'color:Red; font-weight:bold;'>Note :</td><td style = 'color:Red;'>This is confidential mail.So Please, keep the privacy of this mail.</td></tr></table>");
+
+                //footer of email
+                ObjBuilder.Append("<table cellpadding='1' cellspacing='2' align='center' width='90%' style='font - family:Arial, Verdana; font - size:11pt;'>");
+                ObjBuilder.Append("<tr><td align='left' valign = 'middle' style = 'Height:50px; font-weight:bold; text-decoration:underline;'>");
+                ObjBuilder.Append("Thanks and Regards,</td></tr><tr><td style = 'font-weight:bold; color:Green;'>HR Team</td></tr><tr><td>hr@chetu.com</td></tr></table>");
+
+                ObjBuilder.Append("</body></html>");
+
+                return ObjBuilder.ToString();
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        /// <summary>
+        /// Send Email Notification method
+        /// </summary>
+        /// <param name="to"></param>
+        /// <param name="subject"></param>
+        /// <param name="body"></param>
+        /// <param name="cc"></param>
+        public void SendNotification(string to, string subject, string body, string cc = "", string bcc  ="")
+        {
+       
+            try
+            {
+                //--All mail setting initialized from app.config
+                using (MailMessage mail = new MailMessage())
+                {
+                    //mail.To.Add("premk@chetu.com");
+                    mail.To.Add(to);
+                    if (!string.IsNullOrEmpty(cc))
+                    {
+                        mail.CC.Add(cc);
+                    }
+                    if (!string.IsNullOrEmpty(bcc))
+                    {
+                        mail.Bcc.Add(bcc);
+                    }
+                    mail.Subject = subject;
+                    mail.Body = body;
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient())
+                    {
+                        smtp.Send(mail);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //---As this is mail send method so not throwing exception to avoid mail send break for futher emails
+                //--Log Error those mails failed
+                Trace.trace.AppendLine("<br/>SendNotification: Error " + ex.Message);
+                Trace.trace.AppendLine("Mail Send member to & CC that failed " + to + " & " + cc);
+                if (EmailCounter < 1)
+                {
+                    EmailCounter += 1;
+                    MessageBox.Show("Error- [SendNotification Error] " + ConfigurationManager.AppSettings["MorningSnapSubject"].ToString()+ ": " + ex.Message);
+                    SendNotification("ajays@chetu.com", "Error- [SendNotification Error] " + ConfigurationManager.AppSettings["MorningSnapSubject"].ToString(),
+                    Trace.trace.ToString());
+                }
+ 
+            }
+        }
+
+
+        #region Delegates
+        delegate void MailSendDelegate(string to, string subject, string body, string cc, string bcc);
+        #endregion
+
+    }
+}
